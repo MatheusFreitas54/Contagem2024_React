3f8476e0db242214ee9ab65ec2d5ecbb985d77e9
Matheus Freitas <matheusrlcm@gmail.com>
2023-03-04 03:26:29 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Title from './componentes/title';
+import Counter from './componentes/Counter';
+import useCountdown from './hooks/useCountdown';
+
+import './App.css';
+
+
+
+function App() {
+  const [day, hour, minute, second] = useCountdown("jan 1, 2024 00:00:00")
+
+  return (
+    <div className="App">
+      <div className="container">
+        <Title title="Contagem regressiva para 2024" />
+        <div className="countdown-container">
+          <Counter title="Dias" number={day} />
+          <Counter title="Horas" number={hour} />
+          <Counter title="Minutos" number={minute} />
+          <Counter title="Segundos" number={second} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
